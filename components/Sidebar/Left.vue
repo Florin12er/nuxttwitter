86c4f533714c1d2e84a6cd78a069d01ee90c9bda
Florin12er <apetreiflorin236@gmail.com>
2024-10-07 01:33:13 +0200

    added tailwind css, prisma and base layout

---
+++ components/Sidebar/Left.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex flex-col h-screen">
+        <div class="p-2 my-2 hover:bg-blue-50 dark:hover:bg-white/20 rounded-full w-min" :class="defaultTransition">
+            <nuxt-link to="/">
+                <div>
+                    <LogoTwitter :width="width" :height="height" className="text-blue-400 dark:text-white" />
+                </div>
+            </nuxt-link>
+        </div>
+    </div>
+</template>
+<script setup>
+const width = ref(30);
+const height = ref(30);
+const { defaultTransition } = useTailwindConfig();
+</script>
